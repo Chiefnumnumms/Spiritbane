ccac52c38d5a808d76667f4f7a568a0f546690a7
khowell86 <84630214+khowell86@users.noreply.github.com>
2023-01-24 21:52:13 -0500

    Update AudioManager.cs
    Added To Audio Manager

--- Spirit Bane/Assets/03_Scripts/Managers/AudioManager.cs
+++ Spirit Bane/Assets/03_Scripts/Managers/AudioManager.cs
@@ -7,10 +7,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 //-------------------------------------------------------------------------
 // This Class Represents The Audio Manager
 public class AudioManager : Singleton<AudioManager>
 {
+    [SerializeField]
+    private AudioMixer mainMixer;
+
+    [SerializeField]
+    private AudioSource musicAudioSource;
+
+    private AudioMixerGroup master;
+    private AudioMixerGroup music;
+    private AudioMixerGroup effects;
+
+    private float minVolume = -16.0f;
+
+    private float masterVolume;
+    public float MasterVolume { get; set; }
+    private float musicVolume;
+    public float MusicVolume { get; set; }
+    private float effectsVolume;
+    public float EffectsVolume { get; set; }
 
 }
