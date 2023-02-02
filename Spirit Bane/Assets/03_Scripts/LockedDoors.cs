0f79e4087ca8fd4d8a91bff9da0405f84eb1355b
Lucas Moreira <73799457+Chiefnumnumms@users.noreply.github.com>
2023-02-01 20:28:17 -0500

    Implemented Script that handles doors that require pickups

---
+++ Spirit Bane/Assets/03_Scripts/LockedDoors.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class LockedDoors : MonoBehaviour
+{
+    [SerializeField]
+    private string keyName;
+
+    private bool hasKey = false;
+
+    public Ray ray;
+    public RaycastHit hit;
+    public float rayLength = 20f;
+    public Transform raycastOrigin;
+
+    [Header("Door Components")]
+    [SerializeField]
+    private GameObject leftDoor;
+    [SerializeField]
+    private GameObject rightDoor;
+    [SerializeField]
+    private GameObject doorLock;
+    [SerializeField]
+    private GameObject doorChain1;
+    [SerializeField]
+    private GameObject doorChain2;
+
+    [SerializeField]
+    private Animator leftDoorAnim;
+    [SerializeField]
+    private Animator rightDoorAnim;
+
+
+    [Header("Fade Time")]
+    [SerializeField]
+    private float fadeSpeed = 1.5f;
+
+    [Header("Players Inventory")]
+    [SerializeField]
+    private ItemPickup playersItems;
+
+
+    private void Update()
+    {
+        if (!hasKey)
+        {
+            CheckInvForKey();
+        }
+        HandleDoorOpening();
+
+
+    }
+
+    private void CheckInvForKey()
+    {
+        GameObject temp = playersItems.items.Find(x => x.name == keyName);
+
+        if (temp != null)
+        {
+            hasKey = true;
+            Debug.Log("It Fucking Works");
+        }
+    }
+
+    private void HandleDoorOpening()
+    {
+        ray = new Ray(raycastOrigin.position, raycastOrigin.forward);
+        if (Physics.Raycast(ray, out hit, rayLength))
+        {
+            if (hit.collider.CompareTag("Door"))
+            {
+                if (Input.GetKeyDown(KeyCode.E) && hasKey)
+                {
+                    Debug.Log("Oh Shit Is That A Key");
+                    this.gameObject.GetComponent<BoxCollider>().enabled = false;
+
+                    doorLock.SetActive(false);
+                    doorChain1.SetActive(false);
+                    doorChain2.SetActive(false);
+
+                    leftDoorAnim.Play("DoorSwingLeft", 0, 0f);
+                    rightDoorAnim.Play("DoorSwingRight", 0, 0f);
+                }
+            }
+        }
+    }
+
+    
+}
